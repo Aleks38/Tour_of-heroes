e912631dcc4d0c1cca1d9f284447f109ee1db62b
Alexy DA SILVA <alexy.da-silva@etu.univ-grenoble-alpes.fr>
2023-11-11 11:37:06 +0100

    trier les heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,12 +10,15 @@ import {HeroInterfaceService} from "../hero-interface.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
+  parametresTri: string[] = ['attaque', 'esquive', 'degats', 'pv'];
+  selectedParametreTri?: string;
 
   constructor(private heroInterfaceService: HeroInterfaceService) {
   }
 
   ngOnInit(): void {
-    this.getHeroes();
+    this.selectedParametreTri = this.parametresTri[0];
+    this.getHeroesOrderBy();
   }
 
   getHeroes(): void {
@@ -23,7 +26,21 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
+  getHeroesOrderBy(): void {
+    this.heroInterfaceService.getHeroes()
+      .subscribe(heroes => {
+        // Triez les héros en fonction du paramètre sélectionné
+        // @ts-ignore
+        this.heroes = heroes.sort((a, b) => b[this.selectedParametreTri] - a[this.selectedParametreTri]);
+      });
+  }
+
   deleteHero(id: string): void {
     this.heroInterfaceService.deleteHero(id);
+  }
+
+  onParametreTriChange(): void {
+    // Réagir au changement du paramètre de tri
+    this.getHeroesOrderBy();
   }
 }
